76fdd79c7ea5ccdfa0b4744a09bdee1dfab96ea8
rhit-koontzis <koontzis@rose-hulman.edu>
2025-02-16 16:30:41 -0500

    Node Arduino WIP

---
+++ Arduino/server_node/server_node.ino
@@ -0,0 +1,200 @@
+#include "dw3000.h"
+
+#include "DFRobot_GNSS.h"
+
+#define I2C_COMMUNICATION  //use I2C for communication, but use the serial port for communication if the line of codes were masked
+
+#ifdef  I2C_COMMUNICATION
+  DFRobot_GNSS_I2C gnss(&Wire ,GNSS_DEVICE_ADDR);
+#else
+/* ---------------------------------------------------------------------------------------------------------------------
+ *    board   |             MCU                | Leonardo/Mega2560/M0 |    UNO    | ESP8266 | ESP32 |  microbit  |   m0  |
+ *     VCC    |            3.3V/5V             |        VCC           |    VCC    |   VCC   |  VCC  |     X      |  vcc  |
+ *     GND    |              GND               |        GND           |    GND    |   GND   |  GND  |     X      |  gnd  |
+ *     RX     |              TX                |     Serial1 TX1      |     5     |   5/D6  |  D2   |     X      |  tx1  |
+ *     TX     |              RX                |     Serial1 RX1      |     4     |   4/D7  |  D3   |     X      |  rx1  |
+ * ----------------------------------------------------------------------------------------------------------------------*/
+/* Baud rate cannot be changed */
+  #if defined(ARDUINO_AVR_UNO) || defined(ESP8266)
+    SoftwareSerial mySerial(4, 5);
+    DFRobot_GNSS_UART gnss(&mySerial ,9600);
+  #elif defined(ESP32)
+    DFRobot_GNSS_UART gnss(&Serial1 ,9600 ,/*rx*/D2 ,/*tx*/D3);
+  #else
+    DFRobot_GNSS_UART gnss(&Serial1 ,9600);
+  #endif
+#endif
+
+#define PIN_RST 15 //27
+#define PIN_IRQ 4 //34
+#define PIN_SS 5 //4
+
+#define RNG_DELAY_MS 1000
+#define TX_ANT_DLY 16385
+#define RX_ANT_DLY 16385
+#define ALL_MSG_COMMON_LEN 10
+#define ALL_MSG_SN_IDX 2
+#define RESP_MSG_POLL_RX_TS_IDX 10
+#define RESP_MSG_RESP_TX_TS_IDX 14
+#define RESP_MSG_TS_LEN 4
+#define POLL_TX_TO_RESP_RX_DLY_UUS 240
+#define RESP_RX_TIMEOUT_UUS 400
+
+/* Default communication configuration. We use default non-STS DW mode. */
+static dwt_config_t config = {
+    5,                /* Channel number. */
+    DWT_PLEN_128,     /* Preamble length. Used in TX only. */
+    DWT_PAC8,         /* Preamble acquisition chunk size. Used in RX only. */
+    9,                /* TX preamble code. Used in TX only. */
+    9,                /* RX preamble code. Used in RX only. */
+    1,                /* 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type */
+    DWT_BR_6M8,       /* Data rate. */
+    DWT_PHRMODE_STD,  /* PHY header mode. */
+    DWT_PHRRATE_STD,  /* PHY header rate. */
+    (129 + 8 - 8),    /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+    DWT_STS_MODE_OFF, /* STS disabled */
+    DWT_STS_LEN_64,   /* STS length see allowed values in Enum dwt_sts_lengths_e */
+    DWT_PDOA_M0       /* PDOA mode off */
+};
+
+uint8_t tx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'W', 'A', 'V', 'E', 0xE0, 0, 0};
+uint8_t rx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'V', 'E', 'W', 'A', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static uint8_t frame_seq_nb = 0;
+static uint8_t rx_buffer[20];
+static uint32_t status_reg = 0;
+static double tof;
+static double distance;
+extern dwt_txconfig_t txconfig_options;
+unsigned long time_current;
+unsigned long time_offset;
+
+void setup()
+{
+  UART_init();
+
+  spiBegin(PIN_IRQ, PIN_RST);
+  spiSelect(PIN_SS);
+
+  delay(2); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
+  
+  Serial.begin(115200);
+  while(!gnss.begin()){
+    Serial.println("NO Deivces !");
+    delay(1000);
+  }
+
+  gnss.enablePower();      // Enable gnss power 
+
+/** Set GNSS to be used 
+ *   eGPS              use gps
+ *   eBeiDou           use beidou
+ *   eGPS_BeiDou       use gps + beidou
+ *   eGLONASS          use glonass
+ *   eGPS_GLONASS      use gps + glonass
+ *   eBeiDou_GLONASS   use beidou +glonass
+ *   eGPS_BeiDou_GLONASS use gps + beidou + glonass
+ */
+  gnss.setGnss(eGPS_BeiDou_GLONASS);
+
+
+  // gnss.setRgbOff();
+  gnss.setRgbOn();
+
+  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
+  {
+    UART_puts("IDLE FAILED\r\n");
+    while (1)
+      ;
+  }
+
+  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
+  {
+    UART_puts("INIT FAILED\r\n");
+    while (1)
+      ;
+  }
+
+  // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
+  dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
+
+  /* Configure DW IC. See NOTE 6 below. */
+  if (dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
+  {
+    UART_puts("CONFIG FAILED\r\n");
+    while (1)
+      ;
+  }
+
+  /* Configure the TX spectrum parameters (power, PG delay and PG count) */
+  dwt_configuretxrf(&txconfig_options);
+
+  dwt_setrxantennadelay(RX_ANT_DLY);
+  dwt_settxantennadelay(TX_ANT_DLY);
+
+  Serial.println("Server Node");
+  Serial.println("Setup over........");
+}
+
+void loop() {
+  ServerLoop();
+
+}
+
+void ServerLoop(){ //Close to identical to anchor (could be with some communication schema modifications) <- probably not conglomeration
+/* Clear local RX buffer to avoid having leftovers from previous receptions  This is not necessary but is included here to aid reading
+   * the RX buffer.
+   * This is a good place to put a breakpoint. Here (after first time through the loop) the local status register will be set for last event
+   * and if a good receive has happened the data buffer will have the data in it, and frame_len will be set to the length of the RX frame. */
+  memset(rx_buffer, 0, sizeof(rx_buffer));
+
+  /* Activate reception immediately. See NOTE 2 below. */
+  dwt_rxenable(DWT_START_RX_IMMEDIATE);
+
+  /* Poll until a frame is properly received or an error/timeout occurs. See NOTE 3 below.
+   * STATUS register is 5 bytes long but, as the event we are looking at is in the first byte of the register, we can use this simplest API
+   * function to access it. */
+  while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & (SYS_STATUS_RXFCG_BIT_MASK | SYS_STATUS_ALL_RX_ERR)))
+  {
+  };
+
+  uint32_t frame_len;
+
+  if (status_reg & SYS_STATUS_RXFCG_BIT_MASK)
+  {
+    /* A frame has been received, copy it to our local buffer. */
+    frame_len = dwt_read32bitreg(RX_FINFO_ID) & RX_FINFO_RXFLEN_BIT_MASK;
+    if (frame_len <= FRAME_LEN_MAX)
+    {
+      dwt_readrxdata(rx_buffer, frame_len - FCS_LEN, 0); /* No need to read the FCS/CRC. */
+    }
+
+    /* Clear good RX frame event in the DW IC status register. */
+    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
+
+    /* Start achor reception states*/
+    if( rx_buffer[0] == 'S'){
+      switch(rx_buffer[1]){
+        case 'T':
+          TxTime();
+      }
+    }
+  }
+  else
+  {
+    /* Clear RX error events in the DW IC status register. */
+    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_ERR);
+  }
+}
+
+void TxTime(){
+ //Send time to roaming node (Can global)
+}
+
+void AnchorInit(){
+  // Has recieved anchor request for server location will communicate it is within range creating server distance of 1 in anchor (Alternative is global general communication (couldwrk (ask)))
+}
+
+void StoreRange(){
+  // Store the ranges accumulated from anchor nodes (sort in groups of three) <- triple connected hash map? (Is that even a thing)
+}
+
